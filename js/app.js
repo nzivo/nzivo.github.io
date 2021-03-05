d50a3242b17ce82a63c4b87e6bcd0a4dd2557411
nzivo <johnnnzivo@gmail.com>
2021-03-05 15:34:59 +0300

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,171 @@
+"use strict";
+
+jQuery(function ($) {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >= 200) {
+      $(".navbar").addClass("fixed-top");
+    } else if ($(this).scrollTop() == 0) {
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
+  function adjustNav() {
+    var winWidth = $(window).width(),
+      dropdown = $(".dropdown"),
+      dropdownMenu = $(".dropdown-menu");
+
+    if (winWidth >= 768) {
+      dropdown.on("mouseenter", function () {
+        $(this).addClass("show").children(dropdownMenu).addClass("show");
+      });
+
+      dropdown.on("mouseleave", function () {
+        $(this).removeClass("show").children(dropdownMenu).removeClass("show");
+      });
+    } else {
+      dropdown.off("mouseenter mouseleave");
+    }
+  }
+
+  $(window).on("resize", adjustNav);
+
+  adjustNav();
+});
+
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 1200,
+        },
+      },
+      color: {
+        value: "#f29f05",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 30,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  }
+);
+
+$(function () {
+  $('a[href="#search"]').on("click", function (event) {
+    event.preventDefault();
+    $("#search").addClass("open");
+    $('#search > form > input[type="search"]').focus();
+  });
+
+  $("#search, #search button.close").on("click keyup", function (event) {
+    if (
+      event.target == this ||
+      event.target.className == "close" ||
+      event.keyCode == 27
+    ) {
+      $(this).removeClass("open");
+    }
+  });
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    return false;
+  });
+});
